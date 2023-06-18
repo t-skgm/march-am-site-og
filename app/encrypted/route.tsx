14a1dbfc5cf5412f0f829450b5889629dc62fd49
t-skgm <skgm0301@gmail.com>
2023-06-18 18:12:16 +0900

    encrypted image with app route

---
+++ app/encrypted/route.tsx
@@ -0,0 +1,82 @@
+import { ImageResponse } from "@vercel/og";
+import { PageConfig } from "next";
+import { verifyToken } from "../../utils/encrypt";
+
+export const config: PageConfig = {
+  runtime: "edge",
+};
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const id = searchParams.get("id");
+    const token = searchParams.get("token");
+
+    const verifiedToken = await verifyToken({ id });
+
+    if (token !== verifiedToken) {
+      return new Response("Invalid", { status: 401 });
+    }
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: "black",
+            backgroundSize: "150px 150px",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              justifyItems: "center",
+            }}
+          >
+            <img
+              alt="Vercel"
+              height={200}
+              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
+              style={{ margin: "0 30px" }}
+              width={232}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              fontSize: 60,
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              color: "white",
+              marginTop: 30,
+              padding: "0 120px",
+              lineHeight: 1.4,
+              whiteSpace: "pre-wrap",
+            }}
+          >
+            ID: {id}
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
---
+++ utils/encrypt.ts
@@ -0,0 +1,35 @@
+import { createHmac } from "node:crypto";
+
+const secret = process.env.ENCRYTPTION_SECRET!;
+
+export const getKey = async () =>
+  crypto.subtle.importKey(
+    "raw",
+    new TextEncoder().encode(secret),
+    { name: "HMAC", hash: { name: "SHA-256" } },
+    false,
+    ["sign"]
+  );
+
+export const getToken = (value: Record<string, unknown>) => {
+  const hmac = createHmac("sha256", secret);
+  hmac.update(JSON.stringify(value));
+  const token = hmac.digest("hex");
+  return token;
+};
+
+export const verifyToken = async (value: Record<string, unknown>) =>
+  toHex(
+    await crypto.subtle.sign(
+      "HMAC",
+      await getKey(),
+      new TextEncoder().encode(JSON.stringify(value))
+    )
+  );
+
+export const toHex = (arrayBuffer: ArrayBuffer): string =>
+  Array.prototype.map
+    .call(new Uint8Array(arrayBuffer), (n: number) =>
+      n.toString(16).padStart(2, "0")
+    )
+    .join("");
